890ee4186bbb422ae63f7fb89781f809902f883c
Roger Moraes <roger.lm.mr@gmail.com>
2020-04-19 14:11:16 -0300

    Add and Delete Repositories. All tests has passed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import api from './services/api'
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(() => {
+    api.get('repositories').then(repo => {
+      setRepositories(repo.data)
+    })
+  }, [])
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title: 'Monje',
+      url: 'https://github.com/rogeraugusto/monje',
+      techs: ['Node.js', 'EJS', 'Python']
+    })
+
+    setRepositories([...repositories, response.data])
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`)
+
+    setRepositories(repositories.filter(repo => repo.id !== id))
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(repo => (
+          <li key={repo.id}>
+            {repo.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+            <button onClick={() => handleRemoveRepository(repo.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
